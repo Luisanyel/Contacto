0e784b5ff24f965c9dc0b87e94da109b92b31aa5
pc <pc@DESKTOP-7NSEKGR>
2023-02-18 20:19:39 -0600

    Agregar archivos de proyecto.

---
+++ Contactos/Contacto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contactos
+{
+    internal class Contacto : Persona
+    {
+		private string numero;
+
+		public string Numero
+		{
+			get { return numero; }
+			set { numero = value; }
+		}
+        public Contacto() : base()
+        {
+            numero = string.Empty;
+        }
+        public Contacto(String nombre, DateTime? fecha, string telefono) : base(nombre, fecha)
+        {
+            this.numero = telefono;
+        }
+        public override string ToString()
+        {
+            return base.ToString() + " " + numero;
+        }
+    }
+}
---
+++ Contactos/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contactos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<Contacto> lista = new List<Contacto>();
+                bool salir = false; Console.WriteLine("¿Cuántos contactos désea guardar?");
+                int i = int.Parse(Console.ReadLine()), a = 0;
+                while (!salir)
+                {
+                    a++;
+                    Contacto x = new Contacto();
+                    Console.Clear();
+                    Console.WriteLine("favor de ingresar nombre");
+                    x.Nombre = Console.ReadLine();
+                    Console.WriteLine("favor de ingresar fecha de nacimiento con el siguiente formato \nAA/MM/DD");
+                    x.Fecha = DateTime.Parse(Console.ReadLine());
+                    Console.WriteLine("favor de ingresar número de telefono");
+                    x.Numero = Console.ReadLine();
+                    lista.Add(x);
+                    if (i == a)
+                    {
+                        salir = true;
+                    }
+                }
+                Console.Clear();
+                Console.WriteLine("NOMBRE--EDAD--NUMERO DE TELEFONO");
+                foreach (Contacto x in lista)
+                {
+                    Console.WriteLine(x.ToString());
+                }
+            }catch(FormatException e) { Console.WriteLine(e.Message); }
+            Console.ReadKey();
+        }
+    }
+}
